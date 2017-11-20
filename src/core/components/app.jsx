382522670d92330e94d36419c4cec9b78473611b
wangkang <kang.wang03>
2017-11-20 10:36:06 +0800

    add menu

--- src/core/components/app.jsx
+++ src/core/components/app.jsx
@@ -1,23 +1,37 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { Layout, Menu, Breadcrumb, Icon } from 'antd'
+import { Layout, Menu, Breadcrumb, Icon, Button, Input, AutoComplete } from 'antd'
 const { Header, Content, Footer, Sider } = Layout
 const SubMenu = Menu.SubMenu
+const Option = AutoComplete.Option;
+
+function onSelect(value) {
+  console.log('onSelect', value);
+}
 export default class App extends React.Component {
 
   constructor(props) {
     super(props)
     this.state = {
       collapsed: false,
+      dataSource: []
     }
   }
-
-
+  handleSearch = (value) => {
+    this.setState({
+      dataSource: !value ? [] : [
+        value,
+        value + value,
+        value + value + value,
+      ],
+    });
+  }
   onCollapse = (collapsed) => {
     console.log(collapsed);
     this.setState({ collapsed });
   }
   render() {
+    const { dataSource } = this.state;
     return (
       <Layout style={{ minHeight: '100vh' }}>
         <Sider
@@ -26,13 +40,30 @@ export default class App extends React.Component {
           onCollapse={this.onCollapse}
         >
           <div className="logo" />
+
+
           <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-            <Menu.Item key="1">
-            <Icon type="user" />
-            <span>nav 1</span>
+            <div className="global-search-wrapper" style={{ width: '100%' }}>
+              <AutoComplete
+                dataSource={dataSource}
+                style={{ width: 200 }}
+                onSelect={onSelect}
+                onSearch={this.handleSearch}
+                placeholder="input here"
+              />
+            </div>
+            <Menu.Item key="1" className="firstMenu">
+              <Icon type="user" />
+              <span>项目介绍</span>
             </Menu.Item>
-            <Menu.Item key="2">nav 2</Menu.Item>
-            <Menu.Item key="3">nav 3</Menu.Item>
+            <Menu.Item key="2">
+              <Icon type="cloud" />
+              <span>文档中心</span>
+            </Menu.Item>
+            <Menu.Item key="3">
+              <Icon type="file" />
+              <span>测试用例</span>
+            </Menu.Item>
           </Menu>
         </Sider>
         <Layout>
@@ -54,6 +85,8 @@ export default class App extends React.Component {
     )
   }
 }
+
+
 
 App.propTypes = {
   getComponent: PropTypes.func.isRequired,
